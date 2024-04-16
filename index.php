a760332dc50b09dae9318797059618ca3342332d
Shahin Moyshan <128284645+vulcanphp@users.noreply.github.com>
2024-04-16 16:06:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,6 @@ define('ROOT_DIR', __DIR__);
 // register the composer auto loader
 require_once __DIR__ . '/vendor/autoload.php';
 
-// init site cache and serve from cache
-$siteCache = SiteCache::setup([
-    'tmp_dir'   => config('app.tmp_dir'),
-    'except'    => ['/admin/*'],
-]);
-
-// serve from cache if production mode
-if (!is_dev()) {
-    $siteCache->serve();
-}
-
 // create a new application instance for this http request
 Application::create(__DIR__)
     ->registerKernel(App\Http\Kernels\AppKernel::class)
